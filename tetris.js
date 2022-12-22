452bb876eabf4a10a812d36fedb0808955ffa317
leticea <nascimento.leticeah@gmail.com>
2022-12-22 14:13:33 -0300

    criando shapes

--- tetris.js
+++ tetris.js
@@ -26,3 +26,40 @@ const framePerSecond = 24;
 const gameSpeed = 5;
 const canvas = document.getElementById("canvas");
 const image = document.getElementById("image");
+const context = canvas.getContext("2d");
+const squareCountX = canvas.width / size;
+const squareCountY = canvas.height / size;
+
+const shapes = [
+  new Tetris(0, 120, [
+    [0, 1, 0],
+    [0, 1, 0],
+    [1, 1, 0],
+  ],
+  new Tetris(0, 120, [
+    [0, 0, 0],
+    [1, 1, 1],
+    [0, 1, 0],
+  ],
+  new Tetris(0, 120, [
+    [0, 1, 0],
+    [0, 1, 0],
+    [1, 1, 0],
+  ],
+  new Tetris(0, 120, [
+    [0, 1, 0],
+    [0, 1, 0],
+    [1, 1, 0],
+  ],
+  new Tetris(0, 120, [
+    [0, 1, 0],
+    [0, 1, 0],
+    [1, 1, 0],
+  ],
+  new Tetris(0, 120, [
+    [0, 1, 0],
+    [0, 1, 0],
+    [1, 1, 0],
+  ],
+  ),
+];
