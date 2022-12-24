5f448631854f9a8fc40a6154ec25d6e1681db1f5
leticea <nascimento.leticeah@gmail.com>
2022-12-24 09:10:08 -0300

    evitando ultrapassar o lado esquerdo

--- tetris.js
+++ tetris.js
@@ -34,7 +34,7 @@ class Tetris {
       for (let j = 0; j < this.template.length; j++) {
         if (this.template[i][j] == 0) continue
         let realX = i + this.getTruncedPosition().x;
-        let realY = i + this.getTruncedPosition().y;
+        let realY = j + this.getTruncedPosition().y;
         if (realX - 1 < 0) {
           return false;
         }
@@ -48,6 +48,20 @@ class Tetris {
   }
 
   checkRight() {
+    for (let i = 0; i < this.template.length; i++) {
+      for (let j = 0; j < this.template.length; j++) {
+        if (this.template[i][j] == 0) continue
+        let realX = i + this.getTruncedPosition().x;
+        let realY = j + this.getTruncedPosition().y;
+        if (realX + 1 >= squareCountX) {
+          return false;
+        }
+
+        if (gameMap[realY][realX - 1].imageX != -1) {
+          return false;
+        }
+      }
+    }
     return true;
   }
 
@@ -84,6 +98,27 @@ class Tetris {
         this.template[i][last] = this.template[last][last - offset]; // right = bottom
         this.template[last][last - offset] = this.template[last - offset][first]; // bottom = left
         this.template[last - offset][first] = top; // left = top
+      }
+    }
+
+    for (let i = 0; i < this.template.length; i++) {
+      for (let j = 0; j < this.template.length; j++) {
+        if (this.template[i][j] == 0) continue
+        let realX = i + this.getTruncedPosition().x;
+        let realY = j + this.getTruncedPosition().y;
+        if (
+          realX < 0 ||
+          realX >= squareCountX ||
+          realY < 0 ||
+          realY >= squareCountY
+        ) {
+          this.template = tempTemplate;
+          return false;
+        }
+
+        if (gameMap[realY][realX - 1].imageX != -1) {
+          return false;
+        }
       }
     }
   }
